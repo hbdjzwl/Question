4dc36ecc309a8c5c7494fa8ffbab7428c10391fc
AiHao <ChinaAih@163.com>
2022-01-27 18:54:00 +0800

    创建项目

---
+++ Source/Question/QuestionGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuestionGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AQuestionGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AQuestionGameMode();
+};
+
+
+
