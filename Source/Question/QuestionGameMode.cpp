81fd77e5cec2bc724dedf5ad0f9f212ac800e365
AiHao <51937748+hbdjzwl@users.noreply.github.com>
2022-01-29 00:23:28 +0800

    修改GameMode

--- Source/Question/QuestionGameMode.cpp
+++ Source/Question/QuestionGameMode.cpp
@@ -7,7 +7,7 @@
 AQuestionGameMode::AQuestionGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/SideScrollerCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
